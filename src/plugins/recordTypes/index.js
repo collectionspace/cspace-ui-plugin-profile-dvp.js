6c6aa7133e3232e6d474ca168dc55cd2a2671544
Michael Ritter <emericaskate15@gmail.com>
2022-11-03 13:24:46 -0600

    DRYD-1036: Disable unused record type

--- src/plugins/recordTypes/index.js
+++ src/plugins/recordTypes/index.js
@@ -1,6 +1,7 @@
 import acquisition from './acquisition';
 import person from './person';
 import intake from './intake';
+import iterationreport from './iterationreport';
 import concept from './concept';
 import collectionobject from './collectionobject';
 
@@ -8,6 +9,7 @@ export default [
   acquisition,
   person,
   intake,
+  iterationreport,
   collectionobject,
   concept,
 ];
